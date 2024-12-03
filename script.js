c1d3e3ac5e668de7e74d901be6a2f849b4de0ce5
nourahto <24820080@stu.mmu.ac.uk>
2024-12-03 11:44:31 +0000

    my website files

---
+++ script.js
@@ -0,0 +1,8 @@
+// Optional JavaScript: Start fresh here
+console.log("Website loaded!");
+// Toggle the dropdown menu
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('hidden');
+  }
+  
